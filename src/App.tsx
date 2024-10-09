bd85b5f658e75ef945b6a516e3c78fa4f43301bc
Dustin Stark <dstark@udel.edu>
2024-10-09 13:15:17 -0400

    names

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        Dustin Stark John Wiseniewski Don Lafferty Zachariah Sollenberger
         </p>
         <a
           className="App-link"
