65b3dd889e0e44b3a530df5ace99ec97ab2751a8
Dustin Stark <dstark@udel.edu>
2024-10-14 19:25:51 -0400

    maybe

--- src/home.tsx
+++ src/home.tsx
@@ -3,22 +3,32 @@ import { Bquestions } from './bquestions';
 import { Dquestions } from './dquestions';
 import { Button } from "react-bootstrap";
 
-
 export function Home(): React.JSX.Element {
     const [page, setPage] = useState<string>("home");
+
+    const renderPage = () => {
+        switch (page) {
+            case "bquestions":
+                return <Bquestions />;
+            case "dquestions":
+                return <Dquestions />;
+            default:
+                return <p>Welcome to the Super Fun Career Quiz! Please select a quiz type.</p>;
+        }
+    };
 
     return (
         <div>
             <header>
                 <h1>Super Fun Career Quiz</h1>
                 <div>
-                <Button onClick={()=>{setPage("bquestions")}}>Basic Questions</Button>
-                <Button onClick={()=>{setPage("dquestions")}}>Detailed Questions</Button>
+                    <Button onClick={() => setPage("bquestions")}>Basic Questions</Button>
+                    <Button onClick={() => setPage("dquestions")}>Detailed Questions</Button>
                 </div>
-                {page === "home"? <Home/> : null}
-         {page === "bquestions"? <Bquestions/>: null}
-        {page === "dquestions"? <Dquestions/>: null}
             </header>
+            <main>
+                {renderPage()}  {/* Render the correct page based on state */}
+            </main>
         </div>
     );
 };
