f9edac2789bc9875dc5aa9f50e311cc455ce7e85
Dustin Stark <dstark@udel.edu>
2024-10-30 12:56:36 -0400

    fixed linting error

--- src/chat.tsx
+++ src/chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import axios from 'axios';
 
 export const GetResponse = async (apikey: string, prompt: string, result: (arg0: string) => void) => {
